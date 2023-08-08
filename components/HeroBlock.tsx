5d2974fc0a12a12395a5723cd82ba585b96481ae
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-08-08 13:40:50 -0400

    animated new hero

--- components/HeroBlock.tsx
+++ components/HeroBlock.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
 
-import CamperSterningImage from '../public/river-schools/camper-sterning.jpg';
 import RiverDramaShotImage from '../public/river-schools/river-drama-shot.jpg';
 import PurplePimplebacksImage from '../public/river-schools/purple-pimplebacks.jpg';
 import IslandImage from '../public/river-schools/island.jpg';
@@ -9,15 +8,29 @@ import FoggyCanoesImage from '../public/river-schools/foggy-canoes.jpg';
 import CanoeAwayImage from '../public/river-schools/canoe-away.jpg';
 import SwimmingPollywogsImage from '../public/pollywogs/swimming-pollywogs.jpg';
 import RappellingDescentImage from '../public/rappelling/rappelling-descent.jpg';
+import CottonwoodFuzziesImage from '../public/river-rangers/cottonwood-fuzzies.jpg';
 import GoingOverEdgeImage from '../public/rappelling/going-over-edge.jpg';
 import WaterfallImage from '../public/little-solomons/waterfall.jpg';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import Button from './Button';
 import Sparkle from './Sparkle';
 
 const HeroBlock: React.FC = () => {
+  const [curImageIndex, setCurImageIndex] = useState(0);
+  const [hasBeenInteracted, setHasBeenInteracted] = useState(false);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (!hasBeenInteracted) {
+        if (curImageIndex === 4) setCurImageIndex(0);
+        else setCurImageIndex(curImageIndex + 1);
+      }
+    }, 5000);
+    return () => clearTimeout(id);
+  }, [curImageIndex, hasBeenInteracted]);
+
   return (
-    <div className="flex flex-col lg+:flex-row pb-16 bg-gradient-to-b from-white to-slate-50 pt-12">
+    <div className="flex flex-col lg+:flex-row pb-16 bg-gradient-to-b from-white to-slate-50 pt-12 overflow-hidden">
       <div className="pl-8 lg:pl-16 xl:pl-24 pt-4 sm:pt-8 lg+:pt-20 xl:pt-20 pr-8 lg:pr-16 xl:pr-16 pb-20 lg+:pb-24 shrink-0 flex flex-col items-center lg+:items-start">
         <h1 className="text-4xl xs:text-5xl md:text-6xl 2xl:text-7xl font-raleway text-center lg+:text-left max-w-2xl lg+:max-w-xl 2xl:max-w-3xl !leading-tight relative">
           <Sparkle
@@ -69,7 +82,15 @@ const HeroBlock: React.FC = () => {
           lifetime, and see the character of our Creator through it all!
         </h3>
         <div className="flex flex-col sm:flex-row sm:items-center self-stretch sm:self-auto mt-8 xl:mt-12 gap-4">
-          <Button type="link" color="primary" shadow size="lg" to="" icon="video-camera">
+          <Button
+            type="link"
+            color="primary"
+            shadow
+            size="lg"
+            to="https://www.youtube.com/watch?v=iJgqtlZu6g8"
+            icon="video-camera"
+            openInNewTab
+          >
             Watch the video
           </Button>
           <Button
@@ -77,7 +98,7 @@ const HeroBlock: React.FC = () => {
             color="secondary"
             shadow
             size="lg"
-            to=""
+            to="/programs"
             icon="arrow-circle-right"
             className="!bg-emerald-100 !text-emerald-700 hover:!bg-emerald-200"
           >
@@ -87,36 +108,67 @@ const HeroBlock: React.FC = () => {
       </div>
       <div className="flex-grow flex flex-col">
         <div className="flex-grow p-8 relative rounded-3xl bg-emerald-100 my-8 lg:my-12 mx-4 xs:mx-8 lg:mx-16 flex flex-col md:flex-row items-center h-72 lg+:h-auto">
-          <Image
-            src={IslandImage}
-            alt={'Island'}
-            className="w-full md:w-3/4 lg+:w-full h-[120%] lg+:h-full object-cover rounded-3xl shadow-xl absolute left-0 lg+:-left-12 top-[-10%] lg+:-top-8"
+          <ProgramFeature
+            index={0}
+            curIndex={curImageIndex}
+            img={IslandImage}
+            title={'River Schools'}
+            slug={'river-schools'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laborum nobis eveniet saepe iure sint placeat at. Necessitatibus quam maxime.'
+            }
           />
-          <div className="absolute mx-4 md:right-8 lg+:-right-8 -bottom-32 md:-bottom-20 lg+:-bottom-8 bg-white rounded-2xl p-4 lg:p-6 shadow-xl max-w-lg">
-            <h4 className="text-xl font-bold">River schools</h4>
-            <p className="text-slate-600 mt-2 text-sm lg:text-base">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laborum
-              nobis eveniet saepe iure sint placeat at. Necessitatibus quam maxime.
-            </p>
-            <div className="flex justify-end">
-              <Button
-                type="link"
-                color="secondary"
-                size="sm"
-                to=""
-                icon="arrow-circle-right"
-                className="mt-4"
-              >
-                Find out more
-              </Button>
-            </div>
-          </div>
+          <ProgramFeature
+            index={1}
+            curIndex={curImageIndex}
+            img={RappellingDescentImage}
+            title={'Rappelling'}
+            slug={'rappelling'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laborum nobis eveniet saepe iure sint placeat at. Necessitatibus quam maxime.'
+            }
+          />
+          <ProgramFeature
+            index={2}
+            curIndex={curImageIndex}
+            img={CottonwoodFuzziesImage}
+            title={'River Rangers'}
+            slug={'river-rangers'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laborum nobis eveniet saepe iure sint placeat at. Necessitatibus quam maxime.'
+            }
+          />
+          <ProgramFeature
+            index={3}
+            curIndex={curImageIndex}
+            img={SwimmingPollywogsImage}
+            title={'Pollywog Floats'}
+            slug={'pollywogs'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laborum nobis eveniet saepe iure sint placeat at. Necessitatibus quam maxime.'
+            }
+          />
+          <ProgramFeature
+            index={4}
+            curIndex={curImageIndex}
+            img={WaterfallImage}
+            title={'Little Solomon Classes'}
+            slug={'little-solomons'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laborum nobis eveniet saepe iure sint placeat at. Necessitatibus quam maxime.'
+            }
+          />
         </div>
         <div className="mr-8 flex justify-center items-center gap-4 mt-32 md:mt-20 lg:mt-16 lg+:mt-4">
-          <div className="w-3 h-3 bg-slate-200 rounded-full shadow-inner hover:bg-slate-300 transition duration-100 hover:scale-110 cursor-pointer" />
-          <div className="w-3 h-3 bg-slate-200 rounded-full shadow-inner hover:bg-slate-300 transition duration-100 hover:scale-110 cursor-pointer" />
-          <div className="w-3 h-3 bg-slate-200 rounded-full shadow-inner hover:bg-slate-300 transition duration-100 hover:scale-110 cursor-pointer" />
-          <div className="w-3 h-3 bg-slate-200 rounded-full shadow-inner hover:bg-slate-300 transition duration-100 hover:scale-110 cursor-pointer" />
+          {[0, 1, 2, 3, 4].map((i) => (
+            <IndexSwitcher
+              key={i}
+              index={i}
+              curIndex={curImageIndex}
+              setCurrentIndex={setCurImageIndex}
+              setHasBeenInteracted={setHasBeenInteracted}
+            />
+          ))}
         </div>
       </div>
     </div>
@@ -124,3 +176,91 @@ const HeroBlock: React.FC = () => {
 };
 
 export default HeroBlock;
+
+interface ProgramFeatureProps {
+  index: number;
+  curIndex: number;
+  img: StaticImageData;
+  title: string;
+  slug: string;
+  description: string;
+}
+
+const ProgramFeature: React.FC<ProgramFeatureProps> = ({
+  index,
+  curIndex,
+  img,
+  title,
+  slug,
+  description,
+}) => {
+  return (
+    <>
+      <Image
+        src={img}
+        alt={'Canoe Creation fun'}
+        className={cx(
+          'w-full md:w-3/4 lg+:w-full h-[120%] lg+:h-full object-cover rounded-3xl shadow-xl absolute left-0 lg+:-left-12 top-[-10%] lg+:-top-8 [transition:500ms]',
+          index < curIndex && `!scale-50 !opacity-0`,
+          index > curIndex && `!left-[50em] !opacity-0`,
+        )}
+        style={{
+          zIndex: index * 10,
+        }}
+        loading="eager"
+      />
+      <div
+        className={cx(
+          'absolute mx-4 md:right-8 lg+:-right-8 -bottom-32 md:-bottom-20 lg+:-bottom-8 bg-white rounded-2xl p-4 lg:p-6 shadow-xl max-w-lg [transition:600ms] shrink-0',
+          index < curIndex && `!opacity-0 !scale-50`,
+          index > curIndex && `!right-[-50em] !opacity-0`,
+        )}
+        style={{
+          zIndex: index * 10 + 1,
+        }}
+      >
+        <h4 className="text-xl font-bold">{title}</h4>
+        <p className="text-slate-600 mt-2 text-sm lg:text-base">{description}</p>
+        <div className="flex justify-end">
+          <Button
+            type="link"
+            color="secondary"
+            size="sm"
+            to={`/programs/${slug}`}
+            icon="arrow-circle-right"
+            className="mt-4"
+          >
+            Find out more
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+interface IndexSwitcherProps {
+  index: number;
+  curIndex: number;
+  setCurrentIndex: (index: number) => void;
+  setHasBeenInteracted: (hasBeenInteracted: boolean) => void;
+}
+
+const IndexSwitcher: React.FC<IndexSwitcherProps> = ({
+  index,
+  curIndex,
+  setCurrentIndex,
+  setHasBeenInteracted,
+}) => {
+  return (
+    <button
+      onClick={() => {
+        setCurrentIndex(index);
+        setHasBeenInteracted(true);
+      }}
+      className={cx(
+        'w-3 h-3 rounded-full hover:bg-slate-300 shadow-inner transition duration-100 hover:scale-110 cursor-pointer',
+        index === curIndex ? `bg-slate-400 hover:!bg-slate-400` : `bg-slate-200`,
+      )}
+    />
+  );
+};
